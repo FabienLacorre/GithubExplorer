b6e4597a2c1aba133d123dba3c67b17df074d4b2
fabien lacorre <fabien.lacorre@yaakadev.com>
2021-10-17 14:02:25 +0200

    style: remove console log

--- pages/Components/search-bar/index.tsx
+++ pages/Components/search-bar/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import styles from "./style.module.css";
 
 const Ticket: React.FunctionComponent<{}> = () => {
-  console.log(styles);
   return (
     <input
       className={styles.inputCustom}
