7c146ca151f421090e4780d44ee905c41ca5f199
fabien lacorre <fabien.lacorre@yaakadev.com>
2021-10-17 18:12:22 +0200

    style: refactor input style

--- pages/Components/search-bar/index.tsx
+++ pages/Components/search-bar/index.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import styles from "./style.module.css";
 
 const Ticket: React.FunctionComponent<{}> = () => {
-  return (
-    <input
-      className={styles.inputCustom}
-      type="text"
-      placeholder="Search ..."
-    />
-  );
+  const style = {
+    height: "36px",
+    width: "100%",
+    borderRadius: "21.5px",
+    border: "1px solid #D7D7D7",
+    paddingLeft: "18px",
+  };
+  return <input style={style} type="text" placeholder="Search ..." />;
 };
 
 export default Ticket;
