425253982c62167e75fb9cc0f9937e1d7c553489
fabien lacorre <fabien.lacorre@yaakadev.com>
2021-10-17 20:58:16 +0200

    feat: small ui improvement (blue scroll bar + hide scroll bar if there is no ticket to display

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,27 +72,27 @@ const HomeContent: React.FunctionComponent<{}> = ({}) => {
         </form>
 
         {/* FETCHED DATAS */}
-        <div className={styles.ticketContainer}>
-          {data && data.length > 0
-            ? data.map((e: any, index: number) => {
-                return (
-                  <Container key={index + "tickets"}>
-                    <Ticket
-                      handlerRedirection={() => {
-                        router.push({
-                          pathname: "/details",
-                          query: { repo: "test" },
-                        });
-                      }}
-                      title={e?.full_name}
-                      subTitle={e?.description}
-                      logoUrl={e?.owner.avatar_url}
-                    />
-                  </Container>
-                );
-              })
-            : null}
-        </div>
+        {data && data.length > 0 && (
+          <div className={styles.ticketContainer}>
+            {data.map((e: any, index: number) => {
+              return (
+                <Container key={index + "tickets"}>
+                  <Ticket
+                    handlerRedirection={() => {
+                      router.push({
+                        pathname: "/details",
+                        query: { repo: "test" },
+                      });
+                    }}
+                    title={e?.full_name}
+                    subTitle={e?.description}
+                    logoUrl={e?.owner.avatar_url}
+                  />
+                </Container>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
