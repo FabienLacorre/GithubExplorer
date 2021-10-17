fa11e31e2d5abd09e17b5d97fc7ce8c5eb45c046
fabien lacorre <fabien.lacorre@yaakadev.com>
2021-10-17 15:46:11 +0200

    feat: create title, sub title detail page

--- pages/details.tsx
+++ pages/details.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import SearchBar from "./Components/search-bar";
-import Ticket from "./Components/ticket";
 import Text from "./Components/basic-element/text";
-import Container from "./Components/basic-element/container";
-import { GREY_COLOR } from "./constants/colors";
 import Layout from "./Components/basic-element/layout";
+import Container from "./Components/basic-element/container";
+import { BLUE_COLOR, GREY_COLOR, LIGHT_GREY_COLOR } from "./constants/colors";
 
 const Details: NextPage = () => {
   return (
@@ -17,7 +15,22 @@ const Details: NextPage = () => {
       </Head>
 
       <Layout>
-        <span>THIS IS A TEST</span>
+        <Container>
+          <Text size={16} bold={true} color={BLUE_COLOR}>
+            Choose another repository
+          </Text>
+        </Container>
+        <Container>
+          <Text size={50} bold={true} color={GREY_COLOR}>
+            Traefik/traefik
+          </Text>
+        </Container>
+
+        <Container>
+          <Text size={20} color={LIGHT_GREY_COLOR}>
+            The cloud native application
+          </Text>
+        </Container>
       </Layout>
     </div>
   );
