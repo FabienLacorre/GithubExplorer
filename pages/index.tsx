4f3a3262a21b9b8dca4230691efbbad2c9922b77
fabien lacorre <fabien.lacorre@yaakadev.com>
2021-10-17 15:36:05 +0200

    style: remove useless homePage file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-
+import SearchBar from "./Components/search-bar";
+import Ticket from "./Components/ticket";
+import Text from "./Components/basic-element/text";
+import Container from "./Components/basic-element/container";
+import { GREY_COLOR } from "./constants/colors";
 import Layout from "./Components/basic-element/layout";
-import HomePage from "./homePage";
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +17,31 @@ const Home: NextPage = () => {
       </Head>
 
       <Layout>
-        <HomePage />
+        <div className={styles.main}>
+          <div style={{ width: 486, textAlign: "left" }}>
+            <Container>
+              <Text size={32} bold={true} color={GREY_COLOR}>
+                Select a repository
+              </Text>
+            </Container>
+
+            <Container>
+              <SearchBar />
+            </Container>
+
+            <Container>
+              <Ticket />
+            </Container>
+
+            <Container>
+              <Ticket />
+            </Container>
+
+            <Container>
+              <Ticket />
+            </Container>
+          </div>
+        </div>
       </Layout>
     </div>
   );
